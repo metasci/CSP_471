b29ff8e36dbf1ec3db7ec118d483735c1ae11b81
Nick <believewithyoureyes@gmail.com>
2018-04-03 15:29:15 -0400

    added rest of page routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * MY INFO
+ */
 router.get('/', function(req, res, next) {
-  res.render('index', {title:"yo dog"});
+  res.render('index');
 });
 
 
 
+
+/**
+ * MY STUDENTS
+ */
+router.get('/students', function(req, res, next) {
+  res.render('students');
+});
+
+
+
+
+/**
+ * MY SCHEDULE
+ */
+router.get('/schedule', function(req, res, next) {
+  res.render('schedule');
+});
+
+
+
+
+/**
+ * CONTACT
+ */
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+
+
+
+
+/**
+ * LOGIN
+ */
 router.get('/login', function(req, res, next) {
   res.locals.layout = 'loginlayout';
   res.render('login');
+});
+
+
+
+/** 
+ * LOGIN PAGE
+ */
+router.post('/login', function(req, res, next) {
+
+  res.redirect('/');
 });
 
 module.exports = router;
